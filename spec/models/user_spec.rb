6547b499b6237ec4317e2b59b116f205467f175c
Dante Ventieri <dantevvp@gmail.com>
2023-05-12 14:53:45 -0300

    resolve merge conflicts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,42 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe "#old?" do
+    context "when the user was created more than a year ago" do
+      let(:user) { create(:user, created_at: 2.years.ago) }
+
+      it "is old" do
+        expect(user.old?).to eq(true)
+      end
+    end
+
+    context "when the user was created less than a year ago" do
+      let(:user) { create(:user, created_at: 2.days.ago) }
+
+      it "is not old" do
+        expect(user.old?).to eq(false)
+      end
+    end
+  end
+
+  describe "#new?" do
+    context "when the user was created more than a year ago" do
+      let(:user) { create(:user, created_at: 2.years.ago) }
+
+      it "is not new" do
+        expect(user.new?).to eq(false)
+      end
+    end
+
+    context "when the user was created less than a year ago" do
+      let(:user) { create(:user, created_at: 2.days.ago) }
+
+      it "is new" do
+        expect(user.new?).to eq(true)
+      end
+    end
+  end
+
   describe "#something" do
     it "does something" do
       user.something
