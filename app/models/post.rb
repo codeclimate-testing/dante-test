1a068376309920265298ad07f4b3edb72d6e90df
Dante Ventieri <dantevvp@gmail.com>
2023-05-18 19:47:59 -0300

    3

--- app/models/post.rb
+++ app/models/post.rb
@@ -33,11 +33,7 @@ class Post < ApplicationRecord
       puts 'this is covered'
       puts 'this is covered'
       puts 'this is covered'
-      puts 'this is covered'
-      puts 'this is covered'
     else
-      puts 'this is not covered'
-      puts 'this is not covered'
       puts 'this is not covered'
       puts 'this is not covered'
       puts 'this is not covered'
