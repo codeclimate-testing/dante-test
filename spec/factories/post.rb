1750fbe143578dc1a0ab49292654cff4d1455a67
Dante Ventieri <dantevvp@gmail.com>
2023-05-18 18:36:23 -0300

    stop the flaky tests

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
     author
-    title { Faker::Quote.singular_siegler }
+    title { "Some title" }
     body { Faker::Quote.jack_handey[0..139] }
   end
 end
